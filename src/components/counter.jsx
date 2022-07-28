1f4548288cd8b4ac1f1087d7f4b210cc2943d2ca
Fabio Coradini <fabiosrc@gmail.com>
2022-07-28 15:49:37 -0300

    Add movie component

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+﻿// eslint-disable-next-line no-unused-vars
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  formatCount = () =>
+    this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  render() {
+    return (
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secundary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default Counter;
